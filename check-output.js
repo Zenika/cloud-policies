bc1998bc5ad7183a4233773c4b61f07b5b1ff2d0
Hugo Wood <hx4.5@free.fr>
2019-07-19 15:39:42 +0200

    list folder in node instead of bash

--- check-output.js
+++ check-output.js
@@ -1,7 +1,16 @@
 const assert = require("assert");
+const fs = require("fs");
+const path = require("path");
+
 process.argv
   .slice(2)
-  .map(file => [file, require(file)])
+  .flatMap(folder =>
+    fs.readdirSync(folder).map(file => path.join(folder, file))
+  )
+  .map(file => [
+    file,
+    JSON.parse(fs.readFileSync(path.join(file + "/resources.json").toString()))
+  ])
   .forEach(([file, resources]) =>
     assert(
       resources.length === 0,
